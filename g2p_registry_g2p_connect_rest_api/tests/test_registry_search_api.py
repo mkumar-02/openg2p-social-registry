44ab4116cc319c1324bfcf23f85ed59189116b92
mkumar-02 <mkumar6@ch.iitr.ac.in>
2024-05-02 14:07:09 +0530

    G2P Connect API: Improved code coverage
    Signed-off-by: mkumar-02 <mkumar6@ch.iitr.ac.in>

--- g2p_registry_g2p_connect_rest_api/tests/test_registry_search_api.py
+++ g2p_registry_g2p_connect_rest_api/tests/test_registry_search_api.py
@@ -2,6 +2,8 @@ import json
 from datetime import datetime, timezone
 from unittest.mock import patch
 
+import requests
+
 from odoo.tests import tagged
 from odoo.tests.common import HttpCase
 
@@ -54,9 +56,13 @@ class TestRegistrySearchAPITestCase(HttpCase):
         self.assertEqual(401, res.status_code)
         self.assertTrue("Missing authorization header.", res.json()["detail"])
 
-    @patch("odoo.addons.g2p_registry_g2p_connect_rest_api.routers.registry_search.verify_auth_token")
-    def test_registry_search_api_token_verification_failure(self, mock_auth_token):
-        mock_auth_token.return_value = False, {}
+    @patch("odoo.addons.g2p_registry_g2p_connect_rest_api.routers.registry_search.requests.get")
+    @patch("odoo.addons.g2p_registry_g2p_connect_rest_api.routers.registry_search.jwt.decode")
+    def test_registry_search_api_invalid_token(self, mock_jwt_decode, mock_requests_get):
+        mock_requests_get.return_value.raise_for_status.side_effect = requests.exceptions.HTTPError(
+            "Invalid JWKS"
+        )
+        mock_jwt_decode.return_value = False, "Invalid token"
 
         res = self.url_open(
             "/api/v1/g2p-connect/registry/sync/search",
@@ -67,9 +73,23 @@ class TestRegistrySearchAPITestCase(HttpCase):
         self.assertEqual(401, res.status_code)
         self.assertTrue("Invalid Access Token", res.json()["detail"])
 
-    @patch("odoo.addons.g2p_registry_g2p_connect_rest_api.routers.registry_search.verify_auth_token")
-    def test_registry_search_api_valid_token(self, mock_auth_token):
-        mock_auth_token.return_value = True, {}
+    @patch("odoo.addons.g2p_registry_g2p_connect_rest_api.routers.registry_search.requests.get")
+    @patch("odoo.addons.g2p_registry_g2p_connect_rest_api.routers.registry_search.jwt.decode")
+    def test_registry_search_api_jwt_decode_exception(self, mock_jwt_decode, mock_requests_get):
+        mock_jwt_decode.side_effect = Exception("JWT decode error")
+
+        res = self.url_open(
+            "/api/v1/g2p-connect/registry/sync/search",
+            data=json.dumps(self.payload),
+            headers={"Authorization": "Bearer valid_token", "content-type": "application/json"},
+        )
+
+        self.assertEqual(401, res.status_code)
+        self.assertTrue("JWT decode error", res.json()["detail"])
+
+    @patch("odoo.addons.g2p_registry_g2p_connect_rest_api.routers.registry_search.requests.get")
+    @patch("odoo.addons.g2p_registry_g2p_connect_rest_api.routers.registry_search.jwt.decode")
+    def test_registry_search_api_valid_token(self, mock_requests_get, mock_jwt_decode):
         res = self.url_open(
             "/api/v1/g2p-connect/registry/sync/search",
             data=json.dumps(self.payload),
