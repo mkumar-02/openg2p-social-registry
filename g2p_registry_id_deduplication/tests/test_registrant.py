e6ec1306cccf40224e0de567578e0d21f34d1a33
Dibik <dibik@openg2p.org>
2024-07-30 19:08:55 +0530

    Test case issue fixed

--- g2p_registry_id_deduplication/tests/test_registrant.py
+++ g2p_registry_id_deduplication/tests/test_registrant.py
@@ -53,8 +53,26 @@ class TestResPartnerIDDeduplication(TransactionCase):
                 "partner_id": self.individual_2.id,
                 "id_type": self.id_type_2.id,
                 "value": 12345,
-                "status": "valid",
-                "description": "Success",
+                "status": "invalid",
+                "description": "Failed",
+            }
+        )
+        self.reg_id_3 = self.reg_id_model.create(
+            {
+                "partner_id": self.group_1.id,
+                "id_type": self.id_type_1.id,
+                "value": 123,
+                "status": "invalid",
+                "description": "Failed",
+            }
+        )
+        self.reg_id_4 = self.reg_id_model.create(
+            {
+                "partner_id": self.group_2.id,
+                "id_type": self.id_type_1.id,
+                "value": 123,
+                "status": "invalid",
+                "description": "Failed",
             }
         )
 
