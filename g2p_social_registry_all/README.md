15168061571cecbe018146d9cb4b22a2211b90ef
vin0dkhichar <abbb61667@gmail.com>
2025-08-07 11:21:59 +0530

    [ADD] g2p_social_registry_all: module to install all social registry components

---
+++ g2p_social_registry_all/README.md
@@ -0,0 +1,3 @@
+# G2P Social Registry All
+
+Refer to https://docs.openg2p.org.
---
+++ g2p_social_registry_all/__manifest__.py
@@ -0,0 +1,26 @@
+# Part of OpenG2P. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "G2P Social Registry All",
+    "category": "G2P",
+    "version": "17.0.1.5.0",
+    "sequence": 1,
+    "author": "OpenG2P",
+    "website": "https://openg2p.org",
+    "license": "LGPL-3",
+    "depends": [
+        "g2p_social_registry",
+        "g2p_draft_publish",
+        "g2p_leaflet_map",
+        "g2p_registry_g2p_connect_rest_api",
+        "g2p_registry_id_deduplication",
+        "g2p_social_registry_dashboard",
+        "g2p_social_registry_model",
+        "g2p_social_registry_proxy_means_test",
+        "g2p_social_registry_theme",
+    ],
+    "data": [],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
